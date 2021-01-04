021e5f9e3bc8b9ffd4369fc5bbc9fd4ed554319d
Romain Van Geit <vangeitromainz@gmail.com>
2021-01-04 10:10:17 +0100

    first commit + login

---
+++ src/main/java/io/HEPL/dossierFinalSysDist/DossierFinalSysDistApplication.java
@@ -0,0 +1,15 @@
+package io.HEPL.dossierFinalSysDist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class DossierFinalSysDistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierFinalSysDistApplication.class, args);
+	}
+
+}
